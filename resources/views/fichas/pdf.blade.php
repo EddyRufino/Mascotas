6a1f945cf6e2b64655d753b9acdcd02330291fc1
Eddy Rufino <eddyjaair@gmail.com>
2021-09-18 10:58:04 -0500

    Refactoring styles pdf

--- resources/views/fichas/pdf.blade.php
+++ resources/views/fichas/pdf.blade.php
@@ -10,12 +10,23 @@
     .circle {
         display: block;
         width: 130px;
+        position: relative;
+        padding-top: 1rem;
+    }
+
+    .header {
+        border: 1px solid black;
+        padding-top: 1rem;
+        padding-right: 1rem;
+        padding-left: 1rem;
+        padding-bottom: .5rem;
+        margin: 0;
+        border-radius: 2px;
     }
 
     .header-title {
         display: inline-block;
-        padding-left: 30%;
-        padding-bottom: .4rem;
+        float: right;
         text-align: center;
         font-size: 14px;
         font-weight: bold;
@@ -30,31 +41,7 @@
     .pet-container {
         width: 45%;
         float: left;
-        margin-top: 3rem;
-    }
-
-    .pet-box {
-        border: 1px solid #111827;
-        width: 100%;
-        height: 30%;
-        position: absolute;
-        border-radius: 4px;
-    }
-
-    .pet-header {
-        background: #F3F4F6;
-        width: 100%;
-        height: 1.5rem;
-        border-radius: 4px;
-        text-align: center;
-        padding: 8px 0;
-        border-bottom: 1px solid #111827;
-        position: absolute;
-        margin-top: 0;
-    }
-
-    .pet-body {
-        padding: 2.5rem 1rem;
+        margin-top: 1rem;
     }
     /** Fin pet Box **/
 
@@ -64,60 +51,15 @@
         float: right;
     }
 
-    .qr-box {
-        border: 1px solid #111827;
-        width: 100%;
-        height: 30%;
-        position: absolute;
-        border-radius: 4px;
-    }
-
-    .qr-header {
-        width: 100%;
-        background: #F3F4F6;
-        height: 1.5rem;
-        border-radius: 4px;
-        text-align: center;
-        padding: 8px 0;
-        border-bottom: 1px solid #111827;
-        margin-top: 0;
-    }
-
-    .qr-body {
-        padding: 2.5rem 1rem;
-    }
-
     /** Fin QR Box **/
 
     /** Inicio User Box **/
     .user-container {
         width: 60%;
         margin: auto;
-        margin-top: 40%;
+        margin-top: 38.3%;
     }
 
-    .user-box {
-        border: 1px solid #111827;
-        width: 100%;
-        height: 30%;
-        position: absolute;
-        border-radius: 4px;
-    }
-
-    .user-header {
-        width: 100%;
-        background: #F3F4F6;
-        height: 1.5rem;
-        border-radius: 4px;
-        text-align: center;
-        padding: 8px 0;
-        border-bottom: 1px solid #111827;
-        margin-top: 0;
-    }
-
-    .user-body {
-        padding: 2.5rem 1rem;
-    }
     /** Fin User Box **/
 
     .qr {
@@ -130,6 +72,29 @@
         margin-top: 1rem;
         text-align: center;
     }
+
+    /** css para todos**/
+    .all-header {
+        border-bottom: 1px solid black;
+        width: 100%;
+        text-align: center;
+        padding: 8px 0;
+        margin-top: 0;
+        height: 1.5rem;
+    }
+
+    .all-box {
+        border: 1px solid black;
+        width: 100%;
+        height: 32%;
+        position: absolute;
+        border-radius: 2px;
+        font-weight: bold;
+    }
+
+    .all-body {
+        padding: 2.5rem 1rem;
+    }
 </style>
 
 <body>
@@ -138,15 +103,15 @@
             alt="Logo Minucipalidad De Castilla"
             class="circle"
         >
-        <span class="text-center header-title font-color">
+        <span class="header-title font-color">
             MUNICIPALIDAD DISTRITAL DE CASTILLA <br> SUBGERENCIA DE COMERCIALIZACION <br> FICHA DE PROPIEDAD ANIMAL
 
         </span>
     </header>
 
     <article class="pet-container">
-        <h3 class="pet-header">Mi Mascota</h3>
-        <section class="pet-body pet-box">
+        <h3 class="all-header">Mi Mascota</h3>
+        <section class="all-body all-box">
             <p>Nombre: {{ $mascota[0]->nombre }}</p>
             <p>Tipo: {{ $mascota[0]->tipo->nombre }}</p>
             <p>Raza: {{ $mascota[0]->raza }}</p>
@@ -163,8 +128,8 @@
     </article>
 
     <article class="qr-container">
-        <h3 class="qr-header">Código QR</h3>
-        <section class="qr-body qr-box">
+        <h3 class="all-header">Código QR</h3>
+        <section class="all-body all-box">
             <div class="qr">
                 <img src="{{ asset('qrcodes/' . $mascota[0]->url . '.svg') }}">
             </div>
@@ -173,8 +138,8 @@
     </article>
 
     <article class="user-container">
-        <h3 class="user-header">Adulto Responsable</h3>
-        <section class="user-body user-box">
+        <h3 class="all-header">Adulto Responsable</h3>
+        <section class="all-body all-box">
             <p>Nombres: {{ $mascota[0]->user->name }}</p>
             <p>Apellidos: {{ $mascota[0]->user->apellidos }}</p>
             <p>D.N.I: {{ $mascota[0]->user->dni }}</p>
